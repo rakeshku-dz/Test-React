d946db1659c6390cfb5510b19052dfe2f990c656
Rakesh Kumar <rakesh.kumar@ixcnew.com>
2019-09-17 14:19:45 +0530

    Folder structure

--- src/container/home/Home.js
+++ src/container/home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Home() {
   return (
     <div className="mainWrapper d-flex flex-column justify-content-around">        
-        <div className="AppContent"><img src={require('../assets/images/testImage.jpg')} alt="Content Image"/></div>        
+        <div className="AppContent"><img src={require('../../assets/images/testImage.jpg')} alt="Content Image"/></div>        
     </div>
   );
 }
